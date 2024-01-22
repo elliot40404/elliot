dde9c5b4081a0abed32be455baeb0a8587da0492
Elliot <67360013+elliot40404@users.noreply.github.com>
2024-01-23 02:47:22 +0530

    batman

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "elliot40404/elliot:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
